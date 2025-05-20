3c85fcf3fa20214f4e4ad04add3ab082352916eb
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-05-20 18:22:26 +0000

    feat: PrimeNg Installed with TailwindCss
    - Added Test Theme Toggler

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element?.classList.toggle('p-dark');
+}
 }
